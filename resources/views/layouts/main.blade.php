6dde859eaa33b098c0b045e092027598cb7cf4ba
Jacob <jtgraham38@gmail.com>
2024-02-07 23:06:27 -0500

    Added alpine and htmx cdn links.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,9 @@
         @vite('resources/css/style.css')
         <script src="https://kit.fontawesome.com/838b689285.js" crossorigin="anonymous"></script>
 
+        <script src="https://unpkg.com/alpinejs" defer></script>
+        <script src="https://unpkg.com/htmx.org@1.9.10" integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" crossorigin="anonymous"></script>
+
     </head>
     <body>
 
