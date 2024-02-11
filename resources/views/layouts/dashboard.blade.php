0bbf47d46d83097f28fd64e707ec9ee1ad493aff
Jacob <jtgraham38@gmail.com>
2024-02-11 18:49:16 -0500

    Added create build button in menu.

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -9,13 +9,23 @@
                 </summary>
 
                 <ul>
-                    <li class="my-1 px-2 hover:bg-slate-400 hover:bg-opacity-50 text-xl">Project 1</li>
-                    <li class="my-1 px-2 hover:bg-slate-400 hover:bg-opacity-50 text-xl">Build 2</li>
-                    <li class="my-1 px-2 hover:bg-slate-400 hover:bg-opacity-50 text-xl">Item 3</li>
+                    <li class="my-1 px-2 hover:bg-slate-400 hover:bg-opacity-50 text-xl cursor-default"> <i class="fa-solid fa-layer-group"></i> Project 1</li>
+                    <li class="my-1 px-2 hover:bg-slate-400 hover:bg-opacity-50 text-xl cursor-default"> <i class="fa-solid fa-layer-group"></i> Build 2</li>
+                    <li class="my-1 px-2 hover:bg-slate-400 hover:bg-opacity-50 text-xl cursor-default"> <i class="fa-solid fa-layer-group"></i> Item 3</li>
+
+                    <li class="my-1 px-2 hover:bg-slate-400 hover:bg-opacity-50 text-xl cursor-default">
+
+                        <button onclick="create_build_modal.showModal();"><i class="fa-solid fa-circle-plus"></i> New Build</button>
+
+                        <x-modal id="create_build_modal">
+                            create build form here
+                        </x-modal>
+                    </li>
                 </ul>
 
             </details>
         </div>
+        
         <br>
         <div>
             <h5>Edit</h5>
