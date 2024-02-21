4f4033ad1e65e515d28c2123f6826c8e67dcc4a7
Jacob <jtgraham38@gmail.com>
2024-02-21 16:36:25 -0500

    Updated builds table.

--- resources/views/projects/builds.blade.php
+++ resources/views/projects/builds.blade.php
@@ -1,38 +1,46 @@
 <div class="card p-2">
-    <h3>{{$project->name}} Builds</h3>
+    <h3 class="inline">{{$project->name}} Builds</h3> <small class="float-end">(Newest at top)</small>
     <hr>
     <table class="table-auto w-1/2 sm:w-full">
         <thead>
             <tr>
-                <th>Build</th>
-                <th>Created</th>
-                <th></th>
+                <th class="text-left">Build</th>
+                <th class="text-left">Created</th>
                 <th></th>
             </tr>
         </thead>
         <tbody>
 
-            @foreach($project->builds as $build)
+            @foreach($project->builds()->orderBy('created_at', 'desc')->get() as $build)
             <tr>
                 <td>{{ $loop->iteration }}</td>
-                <td>{{ $build->created_at->format('Y-m-d') }}</td>
-                <td>
-                    <form action="{{ route('builds.destroy', ['build'=>$build]) }}" method="POST">
+                <td>{{ $build->created_at->format('Y-m-d H:i:s') }}</td>
+                <td class="flex align-items-center">
+                    <form action="{{ route('builds.destroy', ['build'=>$build]) }}" method="POST" class="mr-1">
                         @csrf
                         @method('DELETE')
                         <button type="submit">
-                            <i class="fa-solid fa-trash"></i>
+                            <i class="fa-solid fa-trash hover:text-zinc-400"></i>
                         </button>
                     </form>
+                    <div class="flex flex-col justify-center">
+                        <a href="{{ route('builds.download', ['build'=>$build]) }}">
+                            <i class="fa-solid fa-download hover:text-zinc-400"></i>
+                        </a>
+                    </div>
                 </td>
-                <td>
-                    <a href="{{ route('builds.download', ['build'=>$build]) }}">
-                        <i class="fa-solid fa-download"></i>
-                    </a>
-                </td>
+
             </tr>
             @endforeach
 
         </tbody>
     </table>
+
+    <div class="flex justify-center w-full mt-2">
+        <form action="{{ route('builds.store') }}" method="POST" class="p-2 primary_btn">
+            @csrf
+            <input type="hidden" name="project_id" value={{ $project->id }}>
+            <button type="submit">New Project Build</button>
+        </form>
+    </div>
 </div>
